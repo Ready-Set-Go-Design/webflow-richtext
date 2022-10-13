91ebc2e4702eadbd0f5fe8c78281d798a3c09898
Brian Hogg <brian@hoggworks.com>
2022-10-13 10:37:38 -0400

    Added stylesheet, added the ability to hide and optionally animate element in

--- index.js
+++ index.js
@@ -1,46 +1,65 @@
-$ = jQuery;
+(function () {
+  $ = jQuery;
 
-$(document).ready(function () {
-  formatAllRichText();
-  const inputClass = "#rsg-editor-input";
+  $(document).ready(function () {
+    formatAllRichText();
+    const inputClass = "#rsg-editor-input";
 
-  // this is used for testing;
-  if ($(inputClass).length > 0) {
-    $(inputClass).on("input", function () {
-      formatAllRichText();
-    });
-  }
-});
+    // this is used for testing;
+    if ($(inputClass).length > 0) {
+      $(inputClass).on("input", function () {
+        formatAllRichText();
+      });
+    }
+  });
 
-function formatAllRichText() {
-  let incoming, inputClass;
+  function formatAllRichText() {
+    let incoming, inputClass;
 
-  formatId = ".w-richtext";
-  console.log;
-  inputClass = "#rsg-editor-input";
+    formatId = ".w-richtext";
+    console.log;
+    inputClass = "#rsg-editor-input";
 
-  // only run this if the test input div is present
-  if ($(inputClass).length > 0) {
-    incoming = $(inputClass).html();
-    incoming = incoming.replaceAll("{{", "<");
-    incoming = incoming.replaceAll("}}", ">");
-  }
-
-  $(formatId).each(function () {
-    if ($(inputClass).length === 0) {
-      incoming = $(this).html();
+    // only run this if the test input div is present
+    if ($(inputClass).length > 0) {
+      incoming = $(inputClass).html();
       incoming = incoming.replaceAll("{{", "<");
       incoming = incoming.replaceAll("}}", ">");
     }
-    $(this).html(incoming);
 
-    // remove p tags that are just line breaks
-    let output = $(this).html();
+    $(formatId).each(function () {
+      if ($(inputClass).length === 0) {
+        incoming = $(this).html();
+        incoming = incoming.replaceAll("{{", "<");
+        incoming = incoming.replaceAll("}}", ">");
+      }
+      $(this).html(incoming);
 
-    $(this).html(output.replaceAll("<p><br></p>", ""));
-    $(this).html(output.replaceAll("&nbsp;</p><p>", ""));
-  });
+      // remove p tags that are just line breaks
+      let output = $(this).html();
 
-  // remove empty p tags
-  $(".w-richtext p:empty").remove();
-}
+      $(this).html(output.replaceAll("<p><br></p>", ""));
+      $(this).html(output.replaceAll("&nbsp;</p><p>", ""));
+      $(this).html(output.replaceAll("</p><p>", ""));
+
+      const shouldAnimateOut = $(this).attr("animate") !== undefined;
+      if (shouldAnimateOut) {
+        setTimeout(() => {
+          $(this).attr("rich-text-layout-animation", "true");
+        }, 250);
+      }
+
+      setTimeout(
+        () => {
+          $(this).removeAttr("rich-text-layout-animation");
+          $(this).removeAttr("rich-text-layout");
+          $(this).removeAttr("animate");
+        },
+        shouldAnimateOut ? 500 : 100
+      );
+    });
+
+    // remove empty p tags
+    $(".w-richtext p:empty").remove();
+  }
+})();
