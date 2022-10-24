ad024b19cb6ec9df667296684e2fad949df8eb12
Brian Hogg <brian@hoggworks.com>
2022-10-24 10:32:37 -0400

    Added initial teleport functionality.

--- index.js
+++ index.js
@@ -17,12 +17,14 @@
     let incoming, inputClass;
 
     formatId = ".w-richtext";
-    console.log;
+
     inputClass = "#rsg-editor-input";
 
     // only run this if the test input div is present
     if ($(inputClass).length > 0) {
       incoming = $(inputClass).html();
+      // look for teleports
+      incoming = parseTeleports(incoming);
       incoming = incoming.replaceAll("{{", "<");
       incoming = incoming.replaceAll("}}", ">");
     }
@@ -30,9 +32,13 @@
       if ($(this).attr("rsg-rich-layout") !== undefined) {
         if ($(inputClass).length === 0) {
           incoming = $(this).html();
+
+          // look for teleports
+          incoming = parseTeleports(incoming);
           incoming = incoming.replaceAll("{{", "<");
           incoming = incoming.replaceAll("}}", ">");
         }
+
         $(this).html(incoming);
 
         // remove p tags that are just line breaks
@@ -62,5 +68,34 @@
 
     // remove empty p tags
     $(".w-richtext p:empty").remove();
+  }
+
+  function parseTeleports(src) {
+    const teleportTagPattern = new RegExp("{{s*teleport[^}}]*}}", "gm");
+    const teleportSources = [];
+    const matches = src.match(teleportTagPattern);
+    matches.forEach((tp) => {
+      const tags = tp.match(/\b(\w+)\s*=\s*"(.*?)"/g);
+      tags.forEach((tag) => {
+        const splitTag = tag.split("=");
+
+        if (splitTag[0].toLowerCase() === "src") {
+          let teleportId = splitTag[1].replaceAll('"', "");
+          teleportSources.push($("#" + teleportId).clone());
+        }
+      });
+    });
+
+    let index = 0;
+    teleportSources.forEach((output) => {
+      if (output[0]) {
+        src = src.replace(matches[index], output[0].outerHTML);
+      } else {
+        src = src.replace(matches[index], "");
+      }
+      index++;
+    });
+
+    return src;
   }
 })();
